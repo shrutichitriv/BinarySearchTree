8b2f4235dce319b82fd730a2c5f7cafae071246b
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-20 23:12:54 +0530

    [Shruti] Perform ability to create a BST by adding

---
+++ BinarySearchTree_244/BinarySearchTreeBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_244
+{
+    internal class BinarySearchTreeBuilder<T> where T : IComparable<T>
+    {
+        public T Nodedata { get; set; }
+        public BinarySearchTreeBuilder<T> leftTree { get; set; }
+        public BinarySearchTreeBuilder<T> rightTree { get; set; }
+
+        public BinarySearchTreeBuilder(T nodeData)
+        {
+            this.Nodedata = nodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.Nodedata;
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new BinarySearchTreeBuilder<T>(item);
+                }
+                else
+                {
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new BinarySearchTreeBuilder<T>(item);
+                }
+                else
+                {
+                    this.rightTree.Insert(item);
+                }
+            }
+        }
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.Nodedata.ToString());
+            if (this.rightTree != null)
+            {
+
+                this.rightTree.Display();
+            }
+
+        }
+    }
+}
--- BinarySearchTree_244/Program.cs
+++ BinarySearchTree_244/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BinarySearchTree
+﻿using BinarySearchTree_244;
+
+namespace BinarySearchTree
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree");
+
+            BinarySearchTreeBuilder<int> binarySearchTree = new BinarySearchTreeBuilder<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
         }
     }
 }
