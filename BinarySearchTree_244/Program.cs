20f3957c7f2696f5b44d6c775812789dc3f4e4a2
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-20 23:19:18 +0530

    [Shruti] Perform ability to create a BST Shown in fig

--- BinarySearchTree_244/Program.cs
+++ BinarySearchTree_244/Program.cs
@@ -8,9 +8,26 @@ namespace BinarySearchTree
         {
             Console.WriteLine("Welcome to Binary Search Tree");
 
+            //BinarySearchTreeBuilder<int> binarySearchTree = new BinarySearchTreeBuilder<int>(56);
+            //binarySearchTree.Insert(30);
+            //binarySearchTree.Insert(70);
+            //binarySearchTree.Display();
+
             BinarySearchTreeBuilder<int> binarySearchTree = new BinarySearchTreeBuilder<int>(56);
             binarySearchTree.Insert(30);
             binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+
+            Console.WriteLine("======Binary Tree elements are======");
             binarySearchTree.Display();
         }
     }
